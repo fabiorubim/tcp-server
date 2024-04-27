47f1d922027d0bbc653af00f850ec7f96123fce3
Fabio Biasi Mello Rubim <fabiorubim@hotmail.com>
2024-04-27 16:34:39 -0300

    Change 2

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,75 +1,93 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
+	"io"
 	"net"
+	"net/http"
+	"os"
 )
 
-// Server ...
-type Server struct {
-	host string
-	port string
-}
+func messageHandler(w http.ResponseWriter, r *http.Request) {
+	// Configura o cabeçalho da resposta para indicar o tipo de conteúdo
+	w.Header().Set("Content-Type", "text/plain")
 
-// Client ...
-type Client struct {
-	conn net.Conn
-}
-
-// Config ...
-type Config struct {
-	Host string
-	Port string
-}
-
-// New ...
-func New(config *Config) *Server {
-	return &Server{
-		host: config.Host,
-		port: config.Port,
-	}
-}
-
-// Run ...
-func (server *Server) Run() {
-	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%s", server.host, server.port))
+	// Lê o corpo da requisição
+	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		log.Fatal(err)
+		http.Error(w, "Erro ao ler o corpo da requisição", http.StatusInternalServerError)
+		return
 	}
-	defer listener.Close()
 
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		client := &Client{
-			conn: conn,
-		}
-		go client.handleRequest()
-	}
-}
-
-func (client *Client) handleRequest() {
-	reader := bufio.NewReader(client.conn)
-	for {
-		message, err := reader.ReadString('\n')
-		if err != nil {
-			client.conn.Close()
-			return
-		}
-		fmt.Printf("Message incoming: %s", string(message))
-		client.conn.Write([]byte("Message received.\n"))
-	}
+	// Converte o corpo para string e escreve na resposta
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Mensagem recebida: %s\n", string(body))
+	fmt.Println("Mensagem:" + string(body))
 }
 
 func main() {
-	server := New(&Config{
-		Host: "0.0.0.0",
-		Port: "8090",
-	})
-	server.Run()
+	privateIP := "0.0.0.0"
+	// Configura o handler para a rota "/"
+	port := "8090"
+	// http.HandleFunc("/", messageHandler)
+
+	// // Inicia o servidor na porta 8080
+	// fmt.Println("Servidor iniciado na porta " + port)
+	// if err := http.ListenAndServe(":"+port, nil); err != nil {
+	// 	fmt.Printf("Erro ao iniciar o servidor: %s\n", err)
+	// }
+
+	// Listen for incoming connections
+	// listener, err := net.Listen("tcp", "54.210.84.139:"+port)
+	listener, err := net.Listen("tcp", privateIP+":"+port)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer listener.Close()
+
+	fmt.Println("Server is listening on port " + port)
+
+	for {
+		// Accept incoming connections
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error:", err)
+			continue
+		}
+
+		// Handle client connection in a goroutine
+		go handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+	// Create a buffer to read data into
+	buffer := make([]byte, 1024)
+
+	for {
+		// Read data from the client
+		n, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		if n > 0 {
+			// Process and use the data (here, we'll just print it)
+			_, err = fmt.Println("Mensagem recebida:", string(buffer[:n]))
+			if err != nil {
+				fmt.Println("Erro ao ler o buffer:", err)
+				return
+			}
+		}
+	}
+}
+
+func getPrivateIP() (string, error) {
+	privateIP := os.Getenv("PRIVATE_IP")
+	if privateIP == "" {
+		return "", fmt.Errorf("variável de ambiente PRIVATE_IP não está definida")
+	}
+	return privateIP, nil
 }
